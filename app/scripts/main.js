553d2c1d8d3ab2500b27f41a6c3573ebebccd85b
Easley, Michael (SLC-MRM) <Michael.Easley@mrmmccann.com>
2014-05-02 17:14:27 -0600

    I almost have the slide thing down.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,5 @@
-console.log('\'Allo \'Allo!');
+$('.search-button button, .push .btn-link').click(function() {
+  $('.swap').toggleClass('transitions');
+  console.log('clicked');
+  $('.push').toggleClass('transitionsTwo');
+});
